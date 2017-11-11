f5ef9e8b401c77bf2278e7c93cf152c62be573ab
Łukasz Grząśko <lukasz.grzasko@hotmail.com>
2017-11-11 23:20:52 +0100

    added server on localhost 3000

--- server.js
+++ server.js
@@ -1,3 +1,14 @@
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+	res.sendFile(__dirname + '/index.html');
+});
+
+
+
+
+http.listen(3000, () => {
+	console.log('listening on *:3000');
+});
